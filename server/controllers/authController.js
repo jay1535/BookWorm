9cc385aef30cc6c45ac39b68d25c899df988aa47
jay1535 <habbujayanth@gmail.com>
2025-08-24 16:13:39 +0530

    started Adding register API : added verification code function in user model and auth controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,52 @@
+import ErrorHandler from "../middlewares/errorMiddlewares.js";
+import { User } from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+
+
+export const register = catchAsyncErrors(async (req, res, next) => {
+try {
+    const {name, email, password} = req.body;
+    if (!name || !email || !password) {
+        return next(new ErrorHandler("Please provide all fields", 400));
+    }
+
+    const isRegistered = await User.findOne({ email, accountVerified: true });
+    if (isRegistered) {
+        return next(new ErrorHandler("User already registered", 400));
+    }
+    const registrationAttemptByUser = await User.find({
+        email,
+        accountVerified: false,
+    });
+    if(registrationAttemptByUser.length >=5) {
+        return next(
+            new ErrorHandler("You have reached the maximum registration attempts. Please try again later.", 400)
+        );
+    }
+    if(password.length < 8 || password.length > 16) {
+        return next(new ErrorHandler("Password must be between 8 and 16 characters", 400));
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+    const verificationCode = await user.generateVerificationCode();
+    await user.save();
+    sendVerificationCode(
+        verificationCode,
+        email,
+         res
+    );
+    
+} 
+
+catch (error) {
+     next(error);
+}
+
+
+})
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -52,4 +52,20 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 })
 
+
+
+userSchema.methods.generateVerificationCode = function(){
+    function generateRandomFiveDigitNumber(){
+        const firstDigit = Math.floor(Math.random()*9)+1;
+        const remainingDigits = Math.floor(Math.random()*10000).toString().padStart(4,0);
+        return parseInt(firstDigit+remainingDigits);
+    }
+    const verificationCode = generateRandomFiveDigitNumber();
+    this.verificationCode = verificationCode;
+
+    this.verificationCodeExpire = Date.now() +15 *60 *1000;
+    return verificationCode;
+
+
+}
 export const User = mongoose.model("User", userSchema);
