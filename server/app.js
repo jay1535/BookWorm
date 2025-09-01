7c3bc8709fb36aedb6331abc3ce109a8743391a5
jay1535 <habbujayanth@gmail.com>
2025-09-01 14:53:12 +0530

    Completed UserController file and userRoutes... also used Cloudinary to store files like userAvatar

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ import { errorMiddleware } from './middlewares/errorMiddlewares.js';
 import authRouter from "./routes/authRouter.js"
 import bookRouter from "./routes/bookRouter.js"
 import borrowRouter from "./routes/borrowRouter.js"
+import fileUpload from "express-fileupload"
+import userRouter from './routes/userRouter.js';
 
 export const app = express();
 config({path:"./config/config.env"});
@@ -22,9 +24,16 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/tmp/"
+}));
+
+
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/book",bookRouter)
 app.use("/api/v1/borrow",borrowRouter)
+app.use("/api/v1/user",userRouter)
 
 
 connectDB();
