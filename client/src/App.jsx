494312bc55ad19ee916cd3fe4fdab1e532e3a1dc
jay1535 <habbujayanth@gmail.com>
2025-09-04 15:54:29 +0530

    Completed adding a initial template for the client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,7 @@
+import React from "react";
 
+const App = () => {
+  return <></>;
+};
 
-function App() {
-  
-
-  return (
-    <>
-      Hi
-    </>
-  )
-}
-
-export default App
+export default App;
