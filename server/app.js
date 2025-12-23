329be6d4af2ef13700936b0066363a46a99110dd
jay1535 <habbujayanth@gmail.com>
2025-12-23 14:02:03 +0530

    Updated user schema

--- server/app.js
+++ server/app.js
@@ -28,11 +28,17 @@ export const app = express();
 ===================================================== */
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: function (origin, callback) {
+      if (!origin || origin === process.env.FRONTEND_URL) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 
 app.use(cookieParser());
 app.use(express.json());
--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,14 +1,18 @@
-export const sendToken = (user, statuscode, message, res) =>{
-    const token = user.generateToken();
+export const sendToken = (user, statusCode, message, res) => {
+  const token = user.getJWTToken();
 
-    
-    res.status(statuscode).cookie("token", token,{
-        expires : new Date(Date.now() + process.env.COOKIE_EXPIRE *24 * 60 * 60 *1000),
-        httpOnly : true
-    }).json({
-        success : true,
-        user,
-        message,
-        token
-    })
-}
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,      
+    sameSite: "None", 
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+  });
+
+  res.status(statusCode).json({
+    success: true,
+    message,
+    user,
+  });
+};
